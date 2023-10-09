65487f3c480e436c2f64460f84d3b1c35ebfdee6
osamaalkarmi20 <osamaalkarmi20@DESKTOP-00FVEU4>
2023-10-09 21:18:18 +0300

    cc

--- cookie-stand-api/Program.cs
+++ cookie-stand-api/Program.cs
@@ -25,24 +25,36 @@ namespace cookie_stand_api
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            //builder.Services.AddCors(options =>
-            //{
-            //    options.AddPolicy("AllowLocalhost3000",
-            //        builder => builder.WithOrigins("http://localhost:3000") // Replace with your Next.js app's URL
-            //                         .AllowAnyHeader()
-            //                         .AllowAnyMethod());
-            //});
+           
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo()
+                {
+                    Title = "Cookie Salmon API",
+                    Version = "v1",
+                });
+            });
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowLocalhost3000",
+                    builder => builder.WithOrigins("http://localhost:3000") // Replace with your Next.js app's URL
+                                     .AllowAnyHeader()
+                                     .AllowAnyMethod());
+            });
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            app.UseSwagger(options =>
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-            //app.UseCors("AllowLocalhost3000");
+                options.RouteTemplate = "swagger/{documentName}/swagger.json";
+            });
+
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Cookie Salmon API V1");
+                options.RoutePrefix = "swagger";
+            });
+            app.UseCors("AllowLocalhost3000");
             app.UseHttpsRedirection();
             
             app.UseAuthorization();
